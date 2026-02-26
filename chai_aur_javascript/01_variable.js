892ba2e54517f38413e5e5b8825d34353036808d
pranavgarg11904586 <pranavg855@gmail.com>
2026-02-26 15:11:06 +0530

    commit

---
+++ chai_aur_javascript/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "pranav@gmail.com";
+var accountPassword = "12345";
+accountCity = "noida";
+
+//accountId=3;
+accountEmail = "hitesh@gmail.com";
+accountPassword = "2121122";
+accountCity = "banglore";
+
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, accountCity]);
