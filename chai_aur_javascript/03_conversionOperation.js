a9b008f30eb7b7b507ba869f30825f96537e0bda
pranavgarg11904586 <pranavg855@gmail.com>
2026-02-26 16:07:13 +0530

    commit

---
+++ chai_aur_javascript/03_conversionOperation.js
@@ -0,0 +1,19 @@
+// string to number conversion
+//let score = "NAN";
+
+// let score = "33abc";
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// number to boolean conversion
+
+//let isLoggedIn = 1; => true
+ //let isLoggedIn = ""; => false
+ //let isLoggedIn = "pranav"; => true
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
