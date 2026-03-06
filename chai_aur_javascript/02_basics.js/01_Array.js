1b0ead87292150b88a21178133f761707c50a11d
pranavgarg11904586 <pranavg855@gmail.com>
2026-03-06 15:33:08 +0530

    commit

---
+++ chai_aur_javascript/02_basics.js/01_Array.js
@@ -0,0 +1,19 @@
+const number = [1,2,3,4];
+
+//console.log(number);
+
+const fruits = new Array("apple","banana","cherry");
+console.log(fruits);
+
+fruits.push("mango");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
+
+fruits.unshift("apple");
+console.log(fruits);
+
