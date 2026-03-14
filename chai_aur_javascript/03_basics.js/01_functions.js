f5e97332d07feb52dcc21ddf6caf70b51c087851
pranavgarg11904586 <pranavg855@gmail.com>
2026-03-14 23:52:57 +0530

    update functions file

---
+++ chai_aur_javascript/03_basics.js/01_functions.js
@@ -0,0 +1,15 @@
+function sayMyName(){
+    console.log("Pranav Garg");
+}
+
+// sayMyName();
+
+function addTwoNumbers(number1, number2){
+
+    return number1 + number2;
+}
+
+const result =addTwoNumbers(5,6);
+
+console.log(result);
+
